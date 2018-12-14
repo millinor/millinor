d0db96ce968fe239e10a1c377fd50c5524c74728
millinor <45846199+millinor@users.noreply.github.com>
2018-12-14 12:04:12 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,11 @@
+function validate form(){
+x=document.get Element By Id("email").value
+if(x=="){
+alert("email must be filled");
+return.false
+}function validate form(){
+x=document.get Element By Id("password").value
+if(x=="){
+alert("password must be filled");
+return.false
+}
