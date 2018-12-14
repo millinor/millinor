59f8f57d2834017f47f0512cbd2ee3d138b6812b
millinor <45846199+millinor@users.noreply.github.com>
2018-12-14 12:01:53 +0530

    Create register.js

---
+++ register.js
@@ -0,0 +1,42 @@
+function validate form(){
+x=document.get Element By Id("user name").value
+if(x=="){
+alert("user name must be filled");
+return false;
+}
+function validate form(){
+x=document.get Element By Id("college name").value
+if(x=="){
+alert("user name must be filled");
+return false;
+}
+function validate form(){
+x=document.get Element By Id("degree").value
+if(x=="){
+alert("degree must be filled");
+return false;
+}
+function validate form(){
+x=document.get Element By Id("language").value
+if(x=="){
+alert("language must be filled");
+return false;
+}
+function validate form(){
+x=document.get Element By Id("email").value
+if(x=="){
+alert("email must be filled");
+return false;
+}
+function validate form(){
+x=document.get Element By Id("password").value
+if(x=="){
+alert("password must be filled");
+return false;
+}
+function validate form(){
+x=document.get Element By Id("repeat password password").value
+if(x=="){
+alert("repeat password must be filled");
+return.false
+}
